f102b646c791e44093bf847ceb78d35e06fb3db1
Laís Rodrigues Martins <navarandadalis@gmail.com>
2022-06-07 18:12:49 -0300

    Componentes & Props & Styled Components - Sta Clara

---
+++ modulo2/labedin/src/components/CardGrande/style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components'
+
+export const Carbig = styled.section`
+    display: flex;
+    flex-direction:justify;
+    align-items: center;
+    border: 1px solid black;
+    padding: 20px 10px;
+    margin-bottom: 10px;
+    width: 440px;
+    height: 200px;
+    
+`
+
+export const CarbigImg = styled.img`
+    width: 70px;
+    margin-right: 10px;
+    border-radius: 50%;
+`
+
+export const CarbigH4 = styled.h4`
+    margin-bottom: 15px;
+`
+
+export const Carbigdiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-items: flex-start;
+`
