a0785f5f9d0b0441d8acde9e24ea880735b53643
Laís Rodrigues Martins <navarandadalis@gmail.com>
2022-07-25 17:10:53 -0300

    recordando labetube

---
+++ modulo3/reforco-jsx-labetube/src/Components/Pages/styled.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Lateral = styled.img`
+width: 70%
+`
+export const Card = styled.div`
+width: 200px;
+height: 250px;  
+border: 1px solid;
+margin: 10px;
+`
+export const Video = styled.img`
+width: 100%
+`
+export const ContainerCard = styled.div`
+display:flex;
+`
+export const ContainerLateral = styled.div`
+border-right: 1px solid;
+`
+
+export const Body = styled.div`
+display: flex;
+`
