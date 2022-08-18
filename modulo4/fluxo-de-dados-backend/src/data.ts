905ff5a827e43ba4cfca8f11e7e2bfa56f499108
Laís Rodrigues Martins <navarandadalis@gmail.com>
2022-08-17 22:11:07 -0300

    fluxo de dados em andamento

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,30 @@
+//Exercício2
+
+
+export type Produtos = {
+    id: string,
+    name: string,
+    price: number
+}
+
+
+export const produtos: Produtos[] = [
+    
+            {
+                id: "zero",
+                name: "dentadura para coelhos velhos",
+                price: 100
+            },
+            {
+                id: "um",
+                name: "lampada de caixão",
+                price: 50
+            },
+            {
+                id: "dois",
+                name: "algemas para fantasmas",
+                price: 30
+            }
+        ]
+    
+    
